4554028271a6beb7944a9c2efe8cae6e24c6b946
GodzillaOO7 <abianandh007@gmail.com>
2023-07-19 07:07:31 +0000

    commit 2

--- angularapp/src/test.ts
+++ angularapp/src/test.ts
@@ -22,8 +22,4 @@ getTestBed().initTestEnvironment(
 // Then we find all the tests.
 const context = require.context('./', true, /\.spec\.ts$/);
 // And load the modules.
-<<<<<<< HEAD
 context.keys().map(context);
-=======
-context.keys().forEach(context);
->>>>>>> 662c039b3c7ea92ea146fee9c1db87fd9fbaf633
