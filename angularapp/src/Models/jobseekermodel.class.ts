48a0251daca915f29067e7192580cdafedefa410
Rajakumaran61 <sakthivel29041969@gmail.com>
2023-07-22 04:06:39 +0000

    Added

--- angularapp/src/Models/jobseekermodel.class.ts
+++
@@ -1,9 +0,0 @@
-export class Jobseekermodel{
-    jobSeekerId!:string;
-    userRole!:string;
-    email!:string;
-    jobSeekerName!:string;
-    address!:string;
-    experience!:string;
-    mobileNumber!:string;
-}
--- angularapp/src/Models/signupmodel.class.ts
+++
@@ -1,7 +0,0 @@
-export class Signupmodel {
-    email!: string;
-    userRole!: string;
-    username!: string;
-    mobileNumber!: string;
-    password!: string;
-    }
